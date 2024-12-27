23a11e7f36769e1f1d458bfd2b855f7537db493a
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-12-27 00:49:26 -0600

    Added home navigation, sliding div

--- portfolio/src/Navigation.jsx
+++ portfolio/src/Navigation.jsx
@@ -2,12 +2,18 @@ function Nav(){
     return (
             <nav>
                 <ul>
-                    <li><a href="#intro">Introduction</a>   
+                    <li>
+                        <a href="#welcome">Home</a>
+                    </li>
+
+                    <li>
+                        <a href="#intro">Introduction</a>   
                     </li>
 
                     <li>
                         <a href="#projects">Projects</a>
                     </li>
+
                     <li>
                         <a href="#">Reach Out</a>
                     </li>
--- portfolio/src/Welcome.jsx
+++ portfolio/src/Welcome.jsx
@@ -1,8 +1,9 @@
 function Welcome() {
     return (
         
-        <div id="welcome"><div className="move">Hello, I am Abdulhafiz.</div>
-            <div>I am an Aspiring Developer.</div>
+        <div id="welcome">
+            <div className="moveLeft">Hello, I am Abdulhafiz.</div>
+            <div className="moveRight">An Aspiring Software Engineer.</div>
             <div><a href="#intro">Learn More About Me</a></div>
         </div>
         
