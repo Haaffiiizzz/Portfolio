47ecec019dcc22ad436a7cae8123d78910793f09
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-01-03 14:26:27 -0600

    added resume link and implemented appearing links

--- portfolio/src/Welcome.jsx
+++ portfolio/src/Welcome.jsx
@@ -4,7 +4,12 @@ function Welcome() {
         <div id="welcome">
             <div className="moveLeft">Hello, I am Abdulhafiz.</div>
             <div className="moveRight">An Aspiring Software Engineer.</div>
-            <div><a href="#about">Learn More About Me</a></div>
+            <div><a href="#about" className="appear">Learn More About Me</a></div>
+            <div id="smallWelcomeLink">
+                <a href="https://docs.google.com/document/d/1iT8PucOE4N3lGe7YkbRKnghtVT6V4CFiIz6xUIlcBdc/edit?usp=sharing" target="_blank">View My Resume</a>
+                <a href="#">Contact Me</a>
+            </div>
+
         </div>
         
     )
